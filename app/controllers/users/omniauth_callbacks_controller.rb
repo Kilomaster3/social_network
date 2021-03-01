26f3aba2630a4e9763d1fe11ea778154952f0734
!kVbP3#BiC%V <grom.4lek@yandex.by>
2021-03-01 12:49:56 +0300

    Added env params

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,7 +5,6 @@ module Users
 
       if @user.persisted?
         sign_in_and_redirect @user, event: :authentication
-        set_flash_message(:notice, :success, kind: "Twitter") if is_navigational_format?
       else
         session["devise.twitter_data"] = request.env["omniauth.auth"].except(:extra)
         redirect_to new_account_registration_url
