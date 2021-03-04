ab75577dcb4d53b986fa03b414ebe4434878998b
!kVbP3#BiC%V <grom.4lek@yandex.by>
2021-03-04 17:33:13 +0300

    Added Friends and Notifications logic
    Added views part of friendship request
    
    Clean up friendship fitch code

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Account < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -5,10 +7,47 @@ class Account < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable
 
-
   has_one_attached :avatar
   has_many :posts
   has_many :likes, dependent: :destroy
   has_many :dislikes
+  has_many :notifications, dependent: :destroy
   mount_uploader :avatar, AvatarUploader
+
+  has_many :friend_sent, class_name: 'Friendship',
+                         foreign_key: 'sent_by_id',
+                         inverse_of: 'sent_by',
+                         dependent: :destroy
+
+  has_many :friend_request, class_name: 'Friendship',
+                            foreign_key: 'sent_to_id',
+                            inverse_of: 'sent_to',
+                            dependent: :destroy
+
+  has_many :friends, -> { merge(Friendship.friends) },
+           through: :friend_sent, source: :sent_to
+
+  has_many :pending_requests, -> { merge(Friendship.not_friends) },
+           through: :friend_sent, source: :sent_to
+
+  has_many :received_requests, -> { merge(Friendship.not_friends) },
+           through: :friend_request, source: :sent_by
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def friends_and_own_posts
+    myfriends = friends
+    our_posts = []
+    myfriends.each do |f|
+      f.posts.each do |p|
+        our_posts << p
+      end
+    end
+    posts.each do |p|
+      our_posts << p
+    end
+    our_posts
+  end
 end
