fa76f0dd38be735d5ec8965dc8f51df218c71675
!kVbP3#BiC%V <grom.4lek@yandex.by>
2021-03-12 17:21:16 +0300

    Fixed problem with sending friend request twice

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,4 +3,5 @@ class Friendship < ApplicationRecord
   belongs_to :sent_by, class_name: 'Account', foreign_key: 'sent_by_id'
   scope :friends, -> { where(status: true) }
   scope :not_friends, -> { where(status: false) }
+  validates :sent_by_id, uniqueness: { scope: :sent_to_id, message: 'can only send one time' }
 end
