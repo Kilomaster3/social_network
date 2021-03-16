20f4005fa12ceb701d1335dfe2551889619a2cea
!kVbP3#BiC%V <grom.4lek@yandex.by>
2021-03-16 20:06:07 +0300

    Fix problems with js

--- app/javascript/src/room.js
+++ app/javascript/src/room.js
@@ -1,5 +1,9 @@
-$(function() {
-  $('#new_room_message').on('ajax:success', function() {
-    $(this).find('input[type="text"]').val('');
-  });
-});
+const bindSendMessageListeners = () => {
+  $(document).on('turbolinks:load', () =>
+    $(document).on('ajax:success', function() {
+      $(this).find('input[type="text"]').val('');
+    })
+  );
+};
+
+export default bindSendMessageListeners;
