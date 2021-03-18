a054ebe65d0b6831007b95512c1c3435a5840d00
!kVbP3#BiC%V <grom.4lek@yandex.by>
2021-03-18 11:17:24 +0300

    Fixed friends request

--- config/initializers/warden_hooks.rb
+++
@@ -1,9 +0,0 @@
-Warden::Manager.after_set_user do |account, auth, opts|
-  scope = opts[:scope]
-  auth.cookies.signed["#{scope}.id"] = account.id
-end
-
-Warden::Manager.before_logout do |auth, opts|
-  scope = opts[:scope]
-  auth.cookies.signed["#{scope}.id"] = nil
-end
