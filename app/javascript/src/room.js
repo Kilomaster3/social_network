568debf206fb00b758f0e6d3aba63b7fcc90cacb
!kVbP3#BiC%V <grom.4lek@yandex.by>
2021-03-18 11:48:49 +0300

    Fix form

--- app/javascript/src/room.js
+++
@@ -1,9 +0,0 @@
-const bindSendMessageListeners = () => {
-  $(document).on('turbolinks:load', () =>
-    $(document).on('ajax:success', function() {
-      $(this).find('input[type="text"]').val('');
-    })
-  );
-};
-
-export default bindSendMessageListeners;
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  resources :room_messages
-  resources :rooms
   devise_for :accounts, controllers: { omniauth_callbacks: 'account/omniauth_callbacks' }
   root to: 'public#home'
   resources :dashboard, only: %i[index create]
