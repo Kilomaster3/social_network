100bedc07cdbf20c37c127c1bfb9cadbba00c58c
!kVbP3#BiC%V <grom.4lek@yandex.by>
2021-04-13 21:24:56 +0300

    Changed location on map

--- app/javascript/src/map.js
+++ app/javascript/src/map.js
@@ -9,11 +9,12 @@ const loader = new Loader({
 
 loader.load().then(() => {
   const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 34.84555, lng: -111.8035 },
+    center: { lat: 53.90339315536, lng: 27.56036829948 },
     zoom: 10,
   });
   const tourStops = [
     [{ lat: gon.locations[0].latitude, lng: gon.locations[0].longitude }, "Bell Rock"],
+    [{ lat: gon.locations[1].latitude, lng: gon.locations[1].longitude }, "AMG"],
   ];
   const infoWindow = new google.maps.InfoWindow();
   tourStops.forEach(([position, title], i) => {
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#10.times do
-# Account.create(
-#   first_name: Faker::Name.first_name,
-#   last_name: Faker::Name.last_name,
-#   email: Faker::Internet.email,
-#   password: '123456789',
-#   password_confirmation: '123456789',
-#   confirmed_at: Time.now.utc
-# )
-#end
+2.times do
+  Account.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: '123456',
+    password_confirmation: '123456',
+    confirmed_at: Time.now.utc,
+    latitude: rand(53.8924818..53.9025719),
+    longitude: rand(27.5782749..27.5474400)
+  )
+end
 
-Tag.create(name: 'Recipe')
-Tag.create(name: 'Travel')
-Tag.create(name: 'News')
-Tag.create(name: 'Humour')
+# {Tag.create(name: 'Recipe')
+# Tag.create(name: 'Travel')
+# Tag.create(name: 'News')
+# Tag.create(name: 'Humour')
 
 
-Interest.create(name: 'Bnw')
-Interest.create(name: 'Mers')
-Interest.create(name: 'Subary')
-Interest.create(name: 'Mazda')
+# Interest.create(name: 'Bnw')
+# Interest.create(name: 'Mers')
+# Interest.create(name: 'Subary')
+# Interest.create(name: 'Mazda')
