3946a2632cfcf32a79757d9d4195c34c768b15fb
!kVbP3#BiC%V <grom.4lek@yandex.by>
2021-04-17 15:55:44 +0300

    Fixed main page
    Fixed main page
    
    Fixed main page
    
    Fixed main page

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,6 +2,11 @@ class PublicController < ApplicationController
   def home
     if current_account
       @accounts = Account.where.not(id: current_account.id)
+      @posts = params[:tag] ? Post.includes(:account).tagged_with(params[:tag]) : Post.includes(:account).all
+      @friends_posts = Post.where(account_id: current_account.following.pluck(:id))
+      @activities = PublicActivity::Activity.order('created_at desc').where(owner_id: current_account.followers, owner_type: 'Account')
+      @likes = Like.where(id: PublicActivity::Activity.where(trackable_type: 'Like').pluck(:trackable_id))
+      @possibile_friends = Account.where.not(id: current_account.followers.pluck(:id))
     end
   end
 end
