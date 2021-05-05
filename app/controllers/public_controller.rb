c6097da2b811bc428b1f4fff82fe5a9750151cbc
Aliaksei Hrom <grom.4lek@yandex.by>
2021-05-05 10:58:42 +0300

    Fix n+1 problem
    Fix n+1 problem

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -4,7 +4,8 @@ class PublicController < ApplicationController
       @accounts = Account.where.not(id: current_account.id)
       @posts = params[:tag] ? Post.includes(:account).tagged_with(params[:tag]) : Post.includes(:account).all
       @friends_posts = Post.where(account_id: current_account.following.pluck(:id))
-      @activities = PublicActivity::Activity.order('created_at desc').where(owner_id: current_account.followers, owner_type: 'Account')
+      @activities = PublicActivity::Activity.order('created_at desc').where(owner_id: current_account.followers,
+                                                                            owner_type: 'Account').includes([:owner]).includes([:trackable])
       @likes = Like.where(id: PublicActivity::Activity.where(trackable_type: 'Like').pluck(:trackable_id))
       @possibile_friends = Account.where.not(id: current_account.followers.pluck(:id)) && Account.where.not(id: current_account.id)
     end
